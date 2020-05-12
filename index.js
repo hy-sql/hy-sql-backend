618fbf16ab92e45d8248eae0e35f83c37434f898
Anssi Lintulampi <anssi.lintulampi@helsinki.fi>
2020-05-12 13:22:06 +0300

    Created simple ping endpoint and added nodemon to dev-dependencies

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ app.get('/', (req,res) => {
     res.send('<h1>Hello World!</h1>')
 })
 
+app.get('/api/ping/', (req, res) => {
+    const json = {value: 'pong'}
+    res.json(json)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log('hello world!')
