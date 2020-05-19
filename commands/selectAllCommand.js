f9e171cb720c04da7ab8e62672dbf44077902fd1
Vivianna Tilander <viva95@windowslive.com>
2020-05-19 22:24:52 +0300

    Select * first draft

--- commands/selectAllCommand.js
+++ commands/selectAllCommand.js
@@ -4,6 +4,28 @@ const isCommand = (fullCommandAsStringList) =>
 
 const execute = (fullCommandAsStringList) => {
     console.log('SELECT *')
+    console.log(fullCommandAsStringList)
+
+    // validate list minsize here and return error if too small
+    const command = parseCommand(fullCommandAsStringList)
+
+    /* validate here when joi validation completed and if errors return error
+   */
+
+    // return command or error
+}
+
+const parseCommand = (fullCommandAsStringList) => {
+    const command = {
+        name: fullCommandAsStringList.slice(0, 2).join(' '),
+        from: fullCommandAsStringList[2],
+        tableName: fullCommandAsStringList[3],
+        finalSemicolon: fullCommandAsStringList[4],
+    }
+
+    console.log(command)
+
+    return command
 }
 
 module.exports = { isCommand, execute }
