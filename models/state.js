5c39d513ef977c2f4d6d8cd5375d38e7c11c273d
Anssi Lintulampi <anssi.lintulampi@helsinki.fi>
2020-05-19 16:15:26 +0300

    state object basic methods started and added unit tests

--- models/state.js
+++ models/state.js
@@ -1,33 +1,49 @@
 class State {
-    constructor(tables) {
-        this.tablelist = tables
-    }
+  constructor(tables) {
+    this.tablelist = tables;
+  }
 
-    get tables() {
-        return this.tablelist
-    }
+  get tables() {
+    return this.tablelist;
+  }
 
-    createTable(command) {
-        if (command.name !== 'CREATE TABLE') {
-            return 'Wrong command type'
-        }
-        const newObject = {
-            name: command.tableName,
-            columns: command.columns,
-            rows: [],
-        }
-        this.tablelist.push(newObject)
+  createTable(command) {
+    if (command.name !== "CREATE TABLE") {
+      return "Wrong command type";
     }
+    const newObject = {
+      name: command.tableName,
+      columns: command.columns,
+      rows: [],
+    };
+    this.tablelist.push(newObject);
+  }
 
-    insertIntoTable(command) {
-        console.log(command)
-        return 'TODO'
+  insertIntoTable(command) {
+    if (command.name !== "INSERT INTO") {
+      return "Wrong command type";
     }
+    const tableIndex = this.tablelist.findIndex(
+      (element) => element.name === command.tableName
+    );
+    let newtablelist = [...this.tablelist];
+    //TODO update wanted columns
+    const newRowObject = {
+      id: newtablelist[tableIndex].rows.length + 1,
+    };
+    newtablelist[tableIndex].rows.push(newRowObject);
+    this.tablelist = newtablelist;
+  }
 
-    selectAllFromTable(command) {
-        console.log(command)
-        return 'TODO'
+  selectAllFromTable(command) {
+    if (command.name !== "SELECT *") {
+      return "Wrong command type";
     }
+    const tableIndex = this.tablelist.findIndex(
+      (element) => element.name === command.tableName
+    );
+    return this.tablelist[tableIndex].rows;
+  }
 }
 
-module.exports = State
+module.exports = State;
---
+++ tests/state.test.js
@@ -0,0 +1,74 @@
+const State = require("../models/state");
+
+test("CREATE TABLE creates new table to list", () => {
+  const initArray = [];
+  const state = new State(initArray);
+  const command = {
+    name: "CREATE TABLE",
+    tableName: "Tuotteet",
+    openingBracket: "(",
+    columns: [
+      { name: "id", type: "INTEGER", primaryKey: true },
+      { name: "nimi", type: "TEXT", primaryKey: false },
+      { name: "hinta", type: "INTEGER", primaryKey: false },
+    ],
+    closingBracket: ")",
+    finalSemicolon: ";",
+  };
+  state.createTable(command);
+  expect(state.tables[0].name).toBe("Tuotteet");
+});
+
+test("INSERT INTO creates row and updates id", () => {
+  const initArray = [];
+  const state = new State(initArray);
+  const createCommand = {
+    name: "CREATE TABLE",
+    tableName: "Tuotteet",
+    openingBracket: "(",
+    columns: [
+      { name: "id", type: "INTEGER", primaryKey: true },
+      { name: "nimi", type: "TEXT", primaryKey: false },
+      { name: "hinta", type: "INTEGER", primaryKey: false },
+    ],
+    closingBracket: ")",
+    finalSemicolon: ";",
+  };
+  state.createTable(createCommand);
+  const insertCommand = {
+    name: "INSERT INTO",
+    tableName: "Tuotteet",
+  };
+  state.insertIntoTable(insertCommand);
+  const rows = state.tables[0].rows;
+  expect(rows[0].id).toBe(1);
+});
+
+test("SELECT * FROM returns rows from table", () => {
+  const initArray = [];
+  const state = new State(initArray);
+  const createCommand = {
+    name: "CREATE TABLE",
+    tableName: "Tuotteet",
+    openingBracket: "(",
+    columns: [
+      { name: "id", type: "INTEGER", primaryKey: true },
+      { name: "nimi", type: "TEXT", primaryKey: false },
+      { name: "hinta", type: "INTEGER", primaryKey: false },
+    ],
+    closingBracket: ")",
+    finalSemicolon: ";",
+  };
+  state.createTable(createCommand);
+  const insertCommand = {
+    name: "INSERT INTO",
+    tableName: "Tuotteet",
+  };
+  state.insertIntoTable(insertCommand);
+  const selectCommand = {
+    name: "SELECT *",
+    tableName: "Tuotteet",
+  };
+  const rows = state.selectAllFromTable(selectCommand);
+  expect(rows.length).toBe(1);
+});
