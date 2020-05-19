4e8f6d8636b0590c326f2fdc3d2ba78921c9e3ce
Artur Shomarov <artur.shomarov@helsinki.fi>
2020-05-19 09:24:05 +0300

    update Command to use switch instead of if

--- commands/Command.js
+++ commands/Command.js
@@ -1,29 +1,29 @@
+/* eslint-disable indent */
 /* eslint-disable no-unused-vars */
 const CreateTableCommand = require('./CreateTableCommand')
 const InsertIntoCommand = require('./InsertIntoCommand')
 const SelectAllCommand = require('./SelectAllCommand')
 
 const Command = (input) => {
-    // const fullCommandAsStringList = input.trim().split(/[\s(())]/)
-    const fullCommandAsStringList = input
-        .trim()
-        .split(/[\s]|(?<=\()|(?=\))|(?=;)/)
+  const fullCommandAsStringList = input
+    .trim()
+    .split(/[\s]|(?<=\()|(?=\))|(?=;)/)
 
-    console.log(fullCommandAsStringList)
+  console.log(fullCommandAsStringList)
 
-    if (fullCommandAsStringList.slice(0, 2).join(' ') === 'CREATE TABLE') {
-        return CreateTableCommand(fullCommandAsStringList)
-    }
+  switch (true) {
+    case fullCommandAsStringList.slice(0, 2).join(' ') === 'CREATE TABLE':
+      return CreateTableCommand(fullCommandAsStringList)
 
-    if (fullCommandAsStringList.slice(0, 2).join(' ') === 'INSERT INTO') {
-        return InsertIntoCommand(fullCommandAsStringList)
-    }
+    case fullCommandAsStringList.slice(0, 2).join(' ') === 'INSERT INTO':
+      return InsertIntoCommand(fullCommandAsStringList)
 
-    if (fullCommandAsStringList.slice(0, 2).join(' ') === 'SELECT *') {
-        return SelectAllCommand(fullCommandAsStringList)
-    }
+    case fullCommandAsStringList.slice(0, 2).join(' ') === 'SELECT *':
+      return SelectAllCommand(fullCommandAsStringList)
 
-    return null
+    default:
+      return null
+  }
 }
 
 module.exports = Command
