7745e4822f613d7b8051cfeca39e9a87c540b3c9
Juha Isopoussu <isopoju@gmail.com>
2020-05-19 20:37:10 +0300

    branch endpoint-sprint1 init commit without husky

---
+++ controllers/queries.js
@@ -0,0 +1,4 @@
+/*
+  POST	/api/query	Vastaanottaa merkkijonoja sisältävän JSON objektin { query: ['...', ..] }
+  GET	/api/query	Palauttaa merkkijonoja sisältävän JSON objektin { result: ['...', ... }
+*/
