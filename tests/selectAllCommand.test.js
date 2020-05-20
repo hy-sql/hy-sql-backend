e3be45f73d67344571af6c2f22f3c5fdc141203e
Vivianna Tilander <viva95@windowslive.com>
2020-05-20 20:26:13 +0300

    Added tests for SelectAllCommand.execute()

--- tests/selectAllCommand.test.js
+++ tests/selectAllCommand.test.js
@@ -1,6 +1,22 @@
 const selectAllCommand = require('../commands/selectAllCommand')
 const selectAllSchema = require('../models/SelectAllSchema')
 
+describe.each(['SELEC * FROM Taulu;', 'SELECT a FROM Taulu;'])(
+    'Query not beginning with SELECT *',
+    (wrongCommand) => {
+        describe(wrongCommand, () => {
+            const command = wrongCommand
+                .trim()
+                .replace(/\s\s+/g, ' ')
+                .split(/[\s]|(?<=\()|(?=\))|(?=;)/)
+
+            test('is not reconised as SELECT * -command', () => {
+                expect(selectAllCommand.isCommand(command)).toBeFalsy()
+            })
+        })
+    }
+)
+
 describe.each([
     'SELECT * FROM Taulu76;',
     'SELECT    * FROM        Taulu;',
@@ -24,24 +40,19 @@ describe.each([
                 selectAllSchema.validate(parsedCommand).error
             ).toBeUndefined()
         })
+
+        test('execute returns command object successfully', () => {
+            expect(selectAllCommand.execute(command)).toHaveProperty('name')
+            expect(selectAllCommand.execute(command)).toHaveProperty('from')
+            expect(selectAllCommand.execute(command)).toHaveProperty(
+                'tableName'
+            )
+            expect(selectAllCommand.execute(command)).toHaveProperty(
+                'finalSemicolon'
+            )
+        })
     })
 })
-
-describe.each(['SELEC * FROM Taulu;', 'SELECT a FROM Taulu;'])(
-    'Query not beginning with SELECT *',
-    (wrongCommand) => {
-        describe(wrongCommand, () => {
-            const command = wrongCommand
-                .trim()
-                .replace(/\s\s+/g, ' ')
-                .split(/[\s]|(?<=\()|(?=\))|(?=;)/)
-
-            test('is not reconised as SELECT * -command', () => {
-                expect(selectAllCommand.isCommand(command)).toBeFalsy()
-            })
-        })
-    }
-)
 
 describe.each([
     'SELECT * FRM Taulu;',
@@ -64,6 +75,15 @@ describe.each([
 
         test('fails validation after parsed to command object', () => {
             expect(selectAllSchema.validate(parsedCommand).error).toBeDefined()
+        })
+
+        test('execute does not return command object', () => {
+            expect(selectAllCommand.execute(command)).not.toHaveProperty(
+                'tableName'
+            )
+            expect(selectAllCommand.execute(command)).not.toHaveProperty(
+                'finalSemicolon'
+            )
         })
     })
 })
