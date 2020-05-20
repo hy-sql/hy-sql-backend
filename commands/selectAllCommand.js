944bb589f9129cea6d3cb03d2121ed5240b75ce4
Vivianna Tilander <viva95@windowslive.com>
2020-05-20 15:41:37 +0300

    Better validation: keywords are case-insesnsitive and table names may now contain underscores andalphanumeric characters

--- commands/selectAllCommand.js
+++ commands/selectAllCommand.js
@@ -2,7 +2,7 @@
 const selectAllSchema = require('../models/SelectAllSchema')
 
 const isCommand = (fullCommandAsStringList) =>
-    fullCommandAsStringList.slice(0, 2).join(' ') === 'SELECT *'
+    fullCommandAsStringList.slice(0, 2).join(' ').toUpperCase() === 'SELECT *'
 
 const execute = (fullCommandAsStringList) => {
     console.log('Input of SELECT * execute:', fullCommandAsStringList)
@@ -11,18 +11,26 @@ const execute = (fullCommandAsStringList) => {
 
     const validationResult = selectAllSchema.validate(command)
     console.log('Validation result:', validationResult)
+    validationResult.error
+        ? console.log('Error type & message:', validationResult.error.details)
+        : ''
 
     // check correct format of returned data
+    // return validationResult is one option. validationResult is either {error: , value:} or {value:}
+    //should in case of error instead of whole error only the details of error be returned? So:  validationResult.error.details
     return validationResult.error
         ? validationResult.error
         : validationResult.value
 }
 
-// Any need to handle missing values for correct error recognition?
 const parseCommand = (fullCommandAsStringList) => {
+    const name = fullCommandAsStringList.slice(0, 2).join(' ')
+
     const command = {
-        name: fullCommandAsStringList.slice(0, 2).join(' '),
-        from: fullCommandAsStringList[2],
+        name: name ? name.toUpperCase() : name,
+        from: fullCommandAsStringList[2]
+            ? fullCommandAsStringList[2].toUpperCase()
+            : fullCommandAsStringList,
         tableName: fullCommandAsStringList[3],
         finalSemicolon: fullCommandAsStringList[4],
     }
--- models/SelectAllSchema.js
+++ models/SelectAllSchema.js
@@ -1,14 +1,15 @@
 const Joi = require('@hapi/joi')
 
 // TODO: Add custom messages to errors and more rules
-// Case-insensitivity in keywords?
+// Case-insensitivity in keywords
+// _ in tablenames in addition to alphanumeric characters
 
 const SelectAllSchema = Joi.object({
     name: Joi.string().required().valid('SELECT *').messages({}),
 
     from: Joi.string().required().valid('FROM').messages({}),
 
-    tableName: Joi.string().required().alphanum().messages({}),
+    tableName: Joi.string().required().pattern(/^\w+$/).messages({}),
 
     finalSemicolon: Joi.string().required().valid(';').messages({}),
 })
