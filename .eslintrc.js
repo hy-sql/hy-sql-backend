bdd992f14bf582557b288f19f27bb756696d11da
Anssi Lintulampi <anssi.lintulampi@helsinki.fi>
2020-05-21 12:54:08 +0300

    added 'SwichCase' to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
         ecmaVersion: 11,
     },
     rules: {
-        indent: ['error', 4],
+        indent: ['error', 4, { SwitchCase: 1 }],
         'linebreak-style': ['error', 'unix'],
         quotes: ['error', 'single'],
         semi: ['error', 'never'],
