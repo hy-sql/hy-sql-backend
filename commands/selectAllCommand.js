332f793f5d6819967cbc1f6b6642625d641f6c24
Vivianna Tilander <viva95@windowslive.com>
2020-05-22 13:48:38 +0300

    Modified SelectAllCommand and it's tests to be compatible with executer.js

--- commands/selectAllCommand.js
+++ commands/selectAllCommand.js
@@ -3,23 +3,15 @@ const selectAllSchema = require('../models/SelectAllSchema')
 const isCommand = (fullCommandAsStringList) =>
     fullCommandAsStringList.slice(0, 2).join(' ').toUpperCase() === 'SELECT *'
 
-const execute = (fullCommandAsStringList) => {
-    const parsedCommand = parseCommand(fullCommandAsStringList)
-
-    const validationResult = selectAllSchema.validate(parsedCommand)
-
-    return validationResult.error ? validationResult.error : parsedCommand
-}
-
 const parseCommand = (fullCommandAsStringList) => {
-    const command = {
+    const parsedCommand = {
         name: fullCommandAsStringList.slice(0, 2).join(' '),
         from: fullCommandAsStringList[2],
         tableName: fullCommandAsStringList[3],
         finalSemicolon: fullCommandAsStringList[4],
     }
 
-    return command
+    return selectAllSchema.validate(parsedCommand)
 }
 
-module.exports = { isCommand, execute, parseCommand }
+module.exports = { isCommand, parseCommand }
