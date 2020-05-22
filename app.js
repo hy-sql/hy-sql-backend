7d446d5b75732486935fedabf5184ac351139e0f
Juha Isopoussu <isopoju@gmail.com>
2020-05-22 11:23:10 +0300

    add morgan http request logger

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const morgan = require('morgan')
+
+morgan.token('body', function (request) { return JSON.stringify(request.body) })
 
 app.use(express.json())
 app.use(cors())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
