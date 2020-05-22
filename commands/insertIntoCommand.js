3325b0d0a05bb1a0ba256b1e0c568700c032236c
sanikk <sanikk@users.noreply.github.com>
2020-05-22 13:42:34 +0300

    ready to merge?

--- commands/insertIntoCommand.js
+++ commands/insertIntoCommand.js
@@ -1,9 +1,73 @@
-/* eslint-disable no-unused-vars */
+const { InsertIntoSchema } = require('../models/InsertIntoSchema')
+
 const isCommand = (fullCommandAsStringList) =>
-    fullCommandAsStringList.slice(0, 2).join(' ') === 'INSERT INTO'
+    fullCommandAsStringList.slice(0, 2).join(' ').toUpperCase() ===
+    'INSERT INTO'
 
 const execute = (fullCommandAsStringList) => {
-    console.log('INSERT INTO')
+    const parsedCommand = parseCommand(fullCommandAsStringList)
+    if (parsedCommand.error) return parsedCommand.error
+    const result = InsertIntoSchema.validate(parsedCommand)
+
+    return !result.error ? parsedCommand : result.error
+}
+
+const parseCommand = (fullCommandAsStringList) => {
+    let anchorLocation = fullCommandAsStringList.indexOf('VALUES')
+
+    if (anchorLocation === -1)
+        return {
+            name: fullCommandAsStringList.slice(0, 2).join(' '),
+            error:
+                'INSERT INTO needs a VALUES keyword before the actual values to be inserted',
+        }
+
+    const columnList = cleanStringArray(
+        fullCommandAsStringList.slice(4, anchorLocation - 1)
+    )
+
+    const command = {
+        name: fullCommandAsStringList.slice(0, 2).join(' '),
+        tableName: fullCommandAsStringList[2],
+        columnsOpeningBracket: fullCommandAsStringList[3],
+        columns: columnList,
+        columnsClosingBracket: fullCommandAsStringList[anchorLocation - 1],
+        anchorKeyword: fullCommandAsStringList[anchorLocation],
+        valuesOpeningBracket: fullCommandAsStringList[anchorLocation + 1],
+        values: addAttributesToValuesArray(
+            columnList,
+            cleanStringArray(
+                fullCommandAsStringList.slice(
+                    anchorLocation + 2,
+                    fullCommandAsStringList.length - 2
+                )
+            )
+        ),
+        valuesClosingBracket:
+            fullCommandAsStringList[fullCommandAsStringList.length - 2],
+        finalSemicolon:
+            fullCommandAsStringList[fullCommandAsStringList.length - 1],
+    }
+
+    return command
+}
+
+const cleanStringArray = (columnsAsStringList) => {
+    return columnsAsStringList.join(' ').split(', ')
+}
+
+const addAttributesToValuesArray = (columnList, stringArray) => {
+    const taulukko = []
+    stringArray.forEach((value, index) => {
+        value.match('[0-9]')
+            ? taulukko.push({
+                column: columnList[index],
+                value,
+                type: 'INTEGER',
+            })
+            : taulukko.push({ column: columnList[index], value, type: 'TEXT' })
+    })
+    return taulukko
 }
 
 module.exports = { isCommand, execute }
---
+++ models/InsertIntoSchema.js
@@ -0,0 +1,41 @@
+const Joi = require('@hapi/joi')
+
+const InsertIntoSchema = Joi.object({
+    name: Joi.string()
+        .min(3)
+        .max(30)
+        .valid('INSERT INTO')
+        .insensitive()
+        .required()
+        .messages({
+            'any.only': 'Command must be "INSERT INTO"',
+        }),
+
+    tableName: Joi.string().alphanum().min(2).max(64).required().messages({
+        'string.base': 'this is not a string',
+    }),
+
+    columnsOpeningBracket: Joi.string().valid('(').required(),
+
+    columns: Joi.array().min(1).required().messages({
+        'array.base': 'this is not an array',
+        'array.min': 'there should be at least one column',
+    }),
+
+    columnsClosingBracket: Joi.string().valid(')').required(),
+
+    anchorKeyword: Joi.string().valid('VALUES').required(),
+
+    valuesOpeningBracket: Joi.string().valid('(').required(),
+
+    values: Joi.array().min(1).required().messages({
+        'array.base': 'this is not an array',
+        'array.min': 'there should be at least one value',
+    }),
+
+    valuesClosingBracket: Joi.string().valid(')').required(),
+
+    finalSemicolon: Joi.string().valid(';').required(),
+})
+
+module.exports = { InsertIntoSchema }
