83948417d157be7e28da0bd9a6aaf90f50fc78e5
Vivianna Tilander <viva95@windowslive.com>
2020-05-26 22:58:20 +0300

    Another addition to SELECT * validation

--- commands/selectAllCommand.js
+++ commands/selectAllCommand.js
@@ -11,7 +11,25 @@ const parseCommand = (fullCommandAsStringList) => {
                 : undefined,
     }
 
-    return selectAllSchema.validate(parsedCommand)
+    const validationResult = selectAllSchema.validate(parsedCommand)
+
+    /* if there is something additional between the table name and ending semicolon
+        an error about the nonbelonging part is created and added to existing
+        validation errors or an error object is added into the validation object */
+    if (fullCommandAsStringList.length > 5) {
+        const additional = fullCommandAsStringList
+            .slice(4, fullCommandAsStringList.length - 1)
+            .join(' ')
+        const errorMessage = `The following part of the query is causing it to fail: '${additional}'`
+
+        validationResult.error
+            ? validationResult.error.details.push({ message: errorMessage })
+            : (validationResult.error = {
+                details: [{ message: errorMessage }],
+            })
+    }
+
+    return validationResult
 }
 
 module.exports = { parseCommand }
