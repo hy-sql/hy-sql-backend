4e62e7f714b1262262e0b0406630dfc01aff49a9
sanikk <sanikk@users.noreply.github.com>
2020-05-28 16:03:54 +0300

    improved parseSelect, changed parseColumn,Table

--- commands/selectCommand.js
+++ commands/selectCommand.js
@@ -6,27 +6,24 @@ const parseCommand = (fullCommandAsStringList) => {
     //KOMENTO
     const parsedCommand = {
         name: fullCommandAsStringList[0],
+        parserCounter: 1
     }
-    let parserCounter = 1
 
     // SARAKKEIDEN OSIO - (*AS -- TODO*)
-    let palautusolio = parseColumnNames(parserCounter, fullCommandAsStringList)
-    parserCounter = palautusolio.parserCounter
-    palautusolio.columnsOpenBrackets > 0
-        ? parsedCommand.columnsOpenBrackets
-        : null
-    parsedCommand.columns = palautusolio.columns
+    parsedCommand = parseColumnNames(parsedCommand, fullCommandAsStringList)
 
     //FROM
-    if (fullCommandAsStringList[parserCounter].toUpperCase() === 'FROM') {
-        parsedCommand.from = fullCommandAsStringList[parserCounter]
-        parserCounter++
+    //tämä olisi helppo muuttaa käymään koko rimpsua läpi, palauttamaan avainsanan, ja heittämään virheen jos avainsana on väärässä paikassa
+    //vs nykyinen, missä katsotaan vain onko se FROM siinä oikeassa paikassa
+    if (fullCommandAsStringList[parsedCommand.parserCounter].toUpperCase() === 'FROM') {
+        parsedCommand.from = fullCommandAsStringList[parsedCommand.parserCounter]
+        parsedCommand.parserCounter++
     }
 
     //TAULUJEN OSIO
-    palautusolio = parseTableNames(parserCounter, fullCommandAsStringList)
-    parserCounter = palautusolio.parserCounter
-    if (palautusolio.tableName) parsedCommand.tableName = palautusolio.tableName
+    parsedCommand = parseTableNames(fullCommandAsStringList, parsedCommand)
+
+
     // WHERE OSIO - specifies which rows to retrieve.
 
     // GROUP BY - groups rows sharing a property so that an aggregate function can be applied to each group.
@@ -36,21 +33,27 @@ const parseCommand = (fullCommandAsStringList) => {
     // ORDER BY -- tehdään controllerissa -- specifies an order in which to return the rows.
 
     // finalSemicolon
-    if (fullCommandAsStringList[parserCounter] === ';')
+    if (fullCommandAsStringList[parsedCommand.parserCounter] === ';') {
         parsedCommand.finalSemicolon = ';'
+        parsedCommand.parserCounter++
 
+    } else if (fullCommandAsStringList[fullCommandAsStringList.length - 1] === ';') {
+        parsedCommand.finalSemicolon = ';'
+        parsedCommand.unparsedBeforeFinalSemicolon = fullCommandAsStringList.slice(parsedCommand.parserCounter, fullCommandAsStringList.length - 2).join(' ')
+    } else {
+        parsedCommand.unparsedBeforeFinalSemicolon = fullCommandAsStringList.slice(parsedCommand.parserCounter, fullCommandAsStringList.length - 1).join(' ')
+    }
     return selectSchema.validate(parsedCommand)
 }
 
-const parseTableNames = (parserCounter, stringArray) => {
-    let palautettava = {
-        parserCounter,
+const parseTableNames = (parserCounter, stringArray, parsedCommand) => {
+
+    //näitä lisää, mieluiten johonkin ReservedWords-listaan joka importataan, *TODO: RESERVED WORDS LIST*
+    if (!['WHERE', 'JOIN', '(', ')', ';', 'VALUES'].includes(stringArray[parserCounter].toUpperCase())) {
+        parsedCommand.parserCounter++
+        parsedCommand.tableName = stringArray[parserCounter]
     }
-    if (stringArray[parserCounter].toUpperCase() !== 'WHERE|JOIN|(|)') {
-        palautettava.parserCounter++
-        palautettava.tableName = stringArray[parserCounter]
-    }
-    return palautettava
+    return parsedCommand
 }
 
 module.exports = { parseCommand }
