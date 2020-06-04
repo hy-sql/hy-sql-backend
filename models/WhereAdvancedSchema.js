b2f6a5032b2b8343b30f66c716ebe330ff57e7c8
Artur Shomarov <shomarov@protonmail.com>
2020-06-04 15:49:50 +0300

    create initial schema for Where conditions

---
+++ models/WhereAdvancedSchema.js
@@ -0,0 +1,13 @@
+const Joi = require('@hapi/joi')
+
+const WhereAdvancedSchema = Joi.object({
+    keyword: Joi.string()
+        .required()
+        .pattern(/;/, { invert: true })
+        .pattern(/^([Ww][Hh][Ee][Rr][Ee])$/)
+        .messages({}),
+
+    conditions: Joi.array(),
+})
+
+module.exports = { WhereAdvancedSchema }
