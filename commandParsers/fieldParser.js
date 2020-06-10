29ee235a517209291118b37967f5828796dba08d
Artur Shomarov <artur.shomarov@helsinki.fi>
2020-06-11 00:27:15 +0300

    refactor parseConditions function

--- commandParsers/fieldParser.js
+++ commandParsers/fieldParser.js
@@ -21,43 +21,41 @@ const parseConditions = (slicedCommandArray) => {
     const indexOfAnd = _.indexOf(conditionArray, 'AND')
     const indexOfOr = _.indexOf(conditionArray, 'OR')
 
-    let AndOrSwitch = 'AND'
-
-    if (indexOfAnd < 0 && indexOfOr >= 0) {
-        AndOrSwitch = 'OR'
-    } else if (indexOfAnd >= 0 && indexOfOr < 0) {
-        AndOrSwitch = 'AND'
-    } else {
-        AndOrSwitch = indexOfAnd <= indexOfOr ? 'AND' : 'OR'
-    }
+    let AndOrSwitch = indexOfOr < (indexOfAnd >= 0) ? 'AND' : 'OR'
 
     for (let i = 0; i < conditionArray.length; i++) {
-        if (conditionArray[i] === '(') {
-            const indexOfClosingBracket = findIndexOfClosingBracket(
-                conditionArray,
-                i
-            )
-            conditions[AndOrSwitch].push(
-                parseConditions(
-                    conditionArray.slice(i + 1, indexOfClosingBracket)
+        switch (conditionArray[i]) {
+            case '(': {
+                const indexOfClosingBracket = findIndexOfClosingBracket(
+                    conditionArray,
+                    i
                 )
-            )
-            i = indexOfClosingBracket + 1
-        } else if (conditionArray[i] === 'AND') {
-            AndOrSwitch = 'AND'
-        } else if (conditionArray[i] === 'OR') {
-            AndOrSwitch = 'OR'
-        } else {
-            const splitExpression = conditionArray[i].split(
-                comparisonOperatorPattern
-            )
-
-            const condition = {
-                left: parseField(splitExpression[0]),
-                operator: splitExpression[1],
-                right: parseField(splitExpression[2]),
+                conditions[AndOrSwitch].push(
+                    parseConditions(
+                        conditionArray.slice(i + 1, indexOfClosingBracket)
+                    )
+                )
+                i = indexOfClosingBracket + 1
+                break
             }
-            conditions[AndOrSwitch].push(condition)
+            case 'AND':
+                AndOrSwitch = 'AND'
+                break
+            case 'OR':
+                AndOrSwitch = 'OR'
+                break
+            default: {
+                const splitExpression = conditionArray[i].split(
+                    comparisonOperatorPattern
+                )
+
+                const condition = {
+                    left: parseField(splitExpression[0]),
+                    operator: splitExpression[1],
+                    right: parseField(splitExpression[2]),
+                }
+                conditions[AndOrSwitch].push(condition)
+            }
         }
     }
 
