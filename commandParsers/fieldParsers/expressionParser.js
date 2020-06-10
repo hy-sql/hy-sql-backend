641527c1941923b0ec8df538ddce3597774172fe
Anssi Lintulampi <anssi.lintulampi@helsinki.fi>
2020-06-10 15:35:56 +0300

    use parseField instead of parseExpressionFields

--- commandParsers/fieldParsers/expressionParser.js
+++ commandParsers/fieldParsers/expressionParser.js
@@ -1,16 +1,16 @@
 const _ = require('lodash')
-// const parseField = require('./fieldParser')
+const parseField = require('./fieldParser')
 const {
     arithmeticOperatorPattern,
     containsFunctionPattern,
-    isArithmeticOperator,
-    stringFunctionPattern,
-    aggregateFunctionPattern,
+    // isArithmeticOperator,
+    // stringFunctionPattern,
+    // aggregateFunctionPattern,
 } = require('../../helpers/regex')
-const {
-    parseParameterFromStringFunction,
-    parseParameterFromAggregateFunction,
-} = require('../parserTools/parseParameterFromFunction')
+// const {
+//     parseParameterFromStringFunction,
+//     parseParameterFromAggregateFunction,
+// } = require('../parserTools/parseParameterFromFunction')
 
 const parseExpression = (expression) => {
     const splitExpression = _.flatten(
@@ -26,46 +26,47 @@ const parseExpression = (expression) => {
             .filter(Boolean)
     )
 
-    return splitExpression.map((e) => parseExpressionFields(e))
+    // return splitExpression.map((e) => parseExpressionFields(e))
+    return splitExpression.map((e) => parseField(e))
 }
 
-const parseExpressionFields = (expressionElement) => {
-    switch (true) {
-        case stringFunctionPattern.test(expressionElement):
-            return {
-                type: 'stringFunction',
-                name: expressionElement.split('(')[0].toUpperCase(),
-                value: expressionElement,
-                param: parseParameterFromStringFunction(expressionElement),
-            }
-        case aggregateFunctionPattern.test(expressionElement):
-            return {
-                type: 'aggregateFunction',
-                name: expressionElement.split('(')[0].toUpperCase(),
-                value: expressionElement,
-                param: parseParameterFromAggregateFunction(expressionElement),
-            }
-        case /^'\w+'/.test(expressionElement):
-            return {
-                type: 'string',
-                value: expressionElement.replace(/'/g, ''),
-            }
-        case !isNaN(expressionElement):
-            return {
-                type: 'integer',
-                value: Number(expressionElement),
-            }
-        case isArithmeticOperator.test(expressionElement):
-            return {
-                type: 'operator',
-                value: expressionElement,
-            }
-        default:
-            return {
-                type: 'column',
-                value: expressionElement,
-            }
-    }
-}
+// const parseExpressionFields = (expressionElement) => {
+//     switch (true) {
+//         case stringFunctionPattern.test(expressionElement):
+//             return {
+//                 type: 'stringFunction',
+//                 name: expressionElement.split('(')[0].toUpperCase(),
+//                 value: expressionElement,
+//                 param: parseParameterFromStringFunction(expressionElement),
+//             }
+//         case aggregateFunctionPattern.test(expressionElement):
+//             return {
+//                 type: 'aggregateFunction',
+//                 name: expressionElement.split('(')[0].toUpperCase(),
+//                 value: expressionElement,
+//                 param: parseParameterFromAggregateFunction(expressionElement),
+//             }
+//         case /^'\w+'/.test(expressionElement):
+//             return {
+//                 type: 'string',
+//                 value: expressionElement.replace(/'/g, ''),
+//             }
+//         case !isNaN(expressionElement):
+//             return {
+//                 type: 'integer',
+//                 value: Number(expressionElement),
+//             }
+//         case isArithmeticOperator.test(expressionElement):
+//             return {
+//                 type: 'operator',
+//                 value: expressionElement,
+//             }
+//         default:
+//             return {
+//                 type: 'column',
+//                 value: expressionElement,
+//             }
+//     }
+// }
 
 module.exports = parseExpression
