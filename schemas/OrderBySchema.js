8081d1dee4b0e0495332b519cedbdfd21c174934
Artur Shomarov <shomarov@protonmail.com>
2020-06-11 12:17:45 +0300

    modify OrderBySchema temporarily to pass validations

--- schemas/OrderBySchema.js
+++ schemas/OrderBySchema.js
@@ -15,22 +15,7 @@ const OrderBySchema = Joi.object({
                 'ORDER BY is either misspelled, missing or in the wrong position',
         }),
 
-    columnName: Joi.string()
-        .pattern(/[;]/, { invert: true })
-        .pattern(/^ASC$|^DESC$/i, { name: 'keywordCheck', invert: true })
-        .pattern(/^\w+$/)
-        .max(64)
-        .required()
-        .messages({
-            'string.pattern.invert.base':
-                'Semicolon should be only found at the end of a query',
-            'string.pattern.base':
-                'Only letters, numbers and underscore allowed in a column name',
-            'string.max': 'A column name is too long',
-            'any.required': 'A column name is missing',
-            'string.pattern.invert.name':
-                'Keywords ASC and DESC must appear after column name',
-        }),
+    columns: Joi.array(),
 
     order: Joi.string()
         .pattern(/[;]/, { invert: true })
