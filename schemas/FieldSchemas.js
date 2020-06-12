182b9cf59d6b06a8f0a2490703f978cf7a2b3d0a
Anssi Lintulampi <anssi.lintulampi@helsinki.fi>
2020-06-12 12:00:18 +0300

    field schema draft

--- schemas/FieldSchemas.js
+++ schemas/FieldSchemas.js
@@ -1,23 +1,39 @@
 const Joi = require('@hapi/joi')
+const {
+    stringFunctionsNamePattern,
+    stringFunctionPattern,
+    // aggregateFunctionsNamePattern,
+    // aggregateFunctionPattern,
+} = require('../helpers/regex')
 
 const AllFieldsSchema = Joi.object({
-    type: Joi.string(),
-    value: Joi.string(),
+    type: Joi.string().valid('all').required(),
+    value: Joi.string().valid('*').required(),
 })
 
 const ColumSchema = Joi.object({
-    type: Joi.string(),
-    value: Joi.string(),
+    type: Joi.string().valid('column').required(),
+    value: Joi.string().pattern(/^\w+$/).required().max(64),
+})
+
+const StringFunctionSchema = Joi.object({
+    type: Joi.string().valid('stringFunction').required(),
+    name: Joi.string().pattern(stringFunctionsNamePattern).required(),
+    value: Joi.string().pattern(stringFunctionPattern).insensitive().required(),
+    param: Joi.object().required(),
 })
 
 const TextSchema = Joi.object({
-    type: Joi.string(),
-    value: Joi.string(),
+    type: Joi.string().valid('TEXT').required(),
+    value: Joi.string().pattern(/^\w+$/).required().max(64),
 })
 
 const IntegerSchema = Joi.object({
-    type: Joi.string(),
-    value: Joi.number(),
+    type: Joi.string().valid('INTEGER').required(),
+    value: Joi.number().required().messages({
+        'any.required':
+            'value type without singlequotes is expected to be number',
+    }),
 })
 
 const ArithmeticOperatorSchema = Joi.object({
@@ -41,6 +57,7 @@ const FunctionSchema = Joi.object()
 module.exports = {
     AllFieldsSchema,
     ColumSchema,
+    StringFunctionSchema,
     TextSchema,
     IntegerSchema,
     ArithmeticOperatorSchema,
