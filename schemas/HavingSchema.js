f8754f8009cf8bbde0eadbc4e963e209d6b2fccf
Juha Isopoussu <isopoju@gmail.com>
2020-06-20 18:43:40 +0300

    fix HavingSchema.js

--- schemas/HavingSchema.js
+++ schemas/HavingSchema.js
@@ -1,24 +1,61 @@
 const Joi = require('@hapi/joi')
-const { ColumnSchema } = require('./FieldSchemas')
-const FunctionSchema = require('./FunctionSchema')
+const { ColumnSchema, IntegerSchema, TextSchema } = require('./FieldSchemas')
 const { ExpressionSchema } = require('./ExpressionSchema')
+const FunctionSchema = require('./FunctionSchema')
+const { comparisonOperatorPattern } = require('../helpers/regex')
 
+/**
+ * Joi schema for validating conditions.
+ */
+const ConditionSchema = Joi.object({
+    left: Joi.alternatives()
+        .try(
+            ColumnSchema,
+            IntegerSchema,
+            TextSchema,
+            ExpressionSchema.shared(FunctionSchema),
+            FunctionSchema.shared(ExpressionSchema)
+        )
+        .required(),
+    operator: Joi.string().pattern(comparisonOperatorPattern).required(),
+    right: Joi.alternatives()
+        .try(
+            ColumnSchema,
+            IntegerSchema,
+            TextSchema,
+            ExpressionSchema.shared(FunctionSchema),
+            FunctionSchema.shared(ExpressionSchema)
+        )
+        .required(),
+}).id('conditionSchema')
+
+/**
+ * Joi schema for validating conditions in HAVING objects.
+ */
+const ConditionsSchema = Joi.object({
+    AND: Joi.link('#conditionsArray'),
+    OR: Joi.link('#conditionsArray'),
+})
+    .required()
+    .id('conditionsSchema')
+
+const conditionsArraySchema = Joi.array()
+    .items(Joi.link('#conditionSchema'), Joi.link('#conditionsSchema'))
+    .id('conditionsArray')
+
+/**
+ * Joi schema for validating HAVING objects. This schema is copy of WhereSchema.
+ * HAVING is like WHERE with aggregate functions.
+ */
 const HavingSchema = Joi.object({
     keyword: Joi.string()
-        .pattern(/[;]/, { invert: true })
+        .pattern(/;/, { invert: true })
         .pattern(/^HAVING$/i)
-        .messages({
-            'any.required':
-                'This query is expected to contain the following keyword: HAVING',
-            'string.pattern.invert.base':
-                'Semicolon should only be found at the end of a query',
-            'string.pattern.base':
-                'HAVING is either misspelled, missing or in the wrong position',
-        }),
+        .required(),
 
-    fields: Joi.array()
-        .items(ColumnSchema, FunctionSchema, ExpressionSchema)
-        .min(1),
+    conditions: ConditionsSchema.shared(
+        conditionsArraySchema.shared(ConditionSchema).shared(ConditionsSchema)
+    ),
 })
 
 module.exports = HavingSchema
