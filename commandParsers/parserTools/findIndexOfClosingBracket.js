6d47339b74bd679382dfa4c424429510f196339b
Artur Shomarov <shomarov@protonmail.com>
2020-06-27 22:44:37 +0300

    edit findIndexOfClosingBracket.js JSDoc, rename variable

--- commandParsers/parserTools/findIndexOfClosingBracket.js
+++ commandParsers/parserTools/findIndexOfClosingBracket.js
@@ -1,17 +1,17 @@
 /**
  * Searches for the index of the closing bracket that matches the opening one
  * at the given index.
- * @param {string[]} conditionsArray array of command conditions
+ * @param {string[]} stringArray array of command conditions
  * @param {Number} IndexOfOpeningBracket index of opening bracket
  * @returns {Number} index of the matching closing bracket
  */
-const findIndexOfClosingBracket = (conditionsArray, IndexOfOpeningBracket) => {
+const findIndexOfClosingBracket = (stringArray, IndexOfOpeningBracket) => {
     const brackets = { opening: 0, closing: 0 }
 
-    for (let i = IndexOfOpeningBracket; i < conditionsArray.length; i++) {
-        if (conditionsArray[i] === '(') {
+    for (let i = IndexOfOpeningBracket; i < stringArray.length; i++) {
+        if (stringArray[i] === '(') {
             brackets.opening = brackets.opening + 1
-        } else if (conditionsArray[i] === ')') {
+        } else if (stringArray[i] === ')') {
             brackets.closing = brackets.closing + 1
         }
 
